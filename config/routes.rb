4c4560e16a4aa537bacd9d7478e2ece0d4fedc70
gnmathew <genemathewdorado@gmail.com>
2023-07-20 16:36:29 +0800

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+  root 'posts#index'
+
+  resources :posts
 
   resources :categories, except: :show
 
