e5edbe0d7f34f23e96dcf28b1519d3a42b9bc9a8
gnmathew <genemathewdorado@gmail.com>
2023-07-20 15:53:28 +0800

    generated Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_051215) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_073555) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -47,6 +47,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_051215) do
 
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
