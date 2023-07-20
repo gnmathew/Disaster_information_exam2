956defa9f8e807a41a18788ced11b54bc1bc9588
gnmathew <genemathewdorado@gmail.com>
2023-07-20 16:52:02 +0800

    generated comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_073555) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_084928) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -51,6 +51,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_073555) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -71,4 +79,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_073555) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
