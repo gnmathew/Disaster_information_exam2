f42604c6a07b7e446757c6430b9d4260eba415e5
gnmathew <genemathewdorado@gmail.com>
2023-07-20 17:19:02 +0800

    added nested routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'posts#index'
 
-  resources :posts
+  resources :posts do
+    resources :comments, except: :show
+  end
 
   resources :categories, except: :show
 
