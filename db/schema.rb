7a8f01fc5d8fce3adf16f7095c80cfe1ea9bdfcc
gnmathew <genemathewdorado@gmail.com>
2023-07-21 21:06:43 +0800

    implemented soft kill

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_021704) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_130349) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -77,6 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_021704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.datetime "deleted_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
