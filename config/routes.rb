2b6a6c66d35234e340df5c82bfa55d431df0ba2e
gnmathew <genemathewdorado@gmail.com>
2023-07-21 13:47:55 +0800

    added namespace for user's comments & posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'posts#index'
+  
+  namespace :user do
+    resources :posts, :comments
+  end
 
   resources :posts do
     resources :comments, except: :show
