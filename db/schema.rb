52b17213229829cae7f91c87c5563085c57d512e
gnmathew <genemathewdorado@gmail.com>
2023-07-21 09:57:52 +0800

    added user_id to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_101138) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_015551) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -74,6 +74,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_101138) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
