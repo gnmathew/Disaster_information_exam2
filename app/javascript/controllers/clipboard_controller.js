5e569676716c3df05f747670c6b359caa2b41b5a
gnmathew <genemathewdorado@gmail.com>
2023-07-22 14:46:07 +0800

    implemented copy to clipboard

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    static targets = ["email"]
+
   copy() {
+    navigator.clipboard.writeText(this.emailTarget.textContent);
+    document.getElementsByClassName("notice")[0].innerText = 'copy email: ' + this.emailTarget.textContent
   }
 }
+import ClipboardController from "./clipboard_controller";
+application.register("clipboard", ClipboardController)
